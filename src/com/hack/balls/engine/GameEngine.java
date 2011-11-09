8edf645abe8805839155f3d440cbae05d5b8517f
Sharad Vikram <sharadv@ubuntu.ubuntu-domain>
2011-11-08 17:21:24 -0800

    initial Ball encapsulation

--- src/com/hack/balls/engine/GameEngine.java
+++ src/com/hack/balls/engine/GameEngine.java
@@ -2,9 +2,14 @@ package com.hack.balls.engine;
 
 public class GameEngine {
 
+	private static void runGame() {
+		
+	}
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World this is Balls of Fury");
-		System.out.println("Sup peoples this is balls of fury.");
+		System.out.println("Hello World  is Balls of Fury");
+		System.out.println("blah blah");
+		
+		GameEngine.runGame();
 	}
 }
