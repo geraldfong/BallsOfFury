9316160d084a2770ae322829b719a4158bdeb03d
Gerald <geraldgfong@gmail.com>
2011-11-08 18:06:00 -0800

    worked on moar panels

--- src/com/hack/balls/engine/GameEngine.java
+++ src/com/hack/balls/engine/GameEngine.java
@@ -1,20 +1,24 @@
 package com.hack.balls.engine;
 
+import javax.swing.JFrame;
+
+import com.hack.balls.swing.MainFrame;
+
 public class GameEngine {
 
 	private static void runGame() {
-		Runnable gameRun = new Runnable() {
+		Runnable swingRun = new Runnable() {
 			public void run() {
-				
+				MainFrame mainFrame = new MainFrame();
+				mainFrame.setVisible(true);
+				mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 		};
-		Thread gameThread = new Thread(gameRun);
-		gameThread.start();
+		Thread swingThread = new Thread(swingRun);
+		swingThread.start();
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World  is Balls of Fury");
-		System.out.println("blah blah");
 		
 		GameEngine.runGame();
 	}
