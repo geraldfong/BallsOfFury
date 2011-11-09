f49177bd11097c5aa26772c5b075980c1cc4519e
Gerald <geraldgfong@gmail.com>
2011-11-08 17:27:39 -0800

    setting up threads

--- src/com/hack/balls/engine/GameEngine.java
+++ src/com/hack/balls/engine/GameEngine.java
@@ -3,7 +3,13 @@ package com.hack.balls.engine;
 public class GameEngine {
 
 	private static void runGame() {
-		
+		Runnable gameRun = new Runnable() {
+			public void run() {
+				
+			}
+		};
+		Thread gameThread = new Thread(gameRun);
+		gameThread.start();
 	}
 	
 	public static void main(String[] args) {
