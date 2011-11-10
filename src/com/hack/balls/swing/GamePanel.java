56454cfe6b92c1a702661b75772d78348dcfaadb
Gerald <geraldgfong@gmail.com>
2011-11-09 18:21:31 -0800

    moar gui

--- src/com/hack/balls/swing/GamePanel.java
+++ src/com/hack/balls/swing/GamePanel.java
@@ -1,19 +1,34 @@
 package com.hack.balls.swing;
 
+import java.awt.Graphics;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.hack.balls.engine.PhysicsEngine;
+
 public class GamePanel extends JPanel{
 
-	JLabel score;
+	private JLabel scoreLabel;
+	private PhysicsEngine physicsEngine;
 	
 	public GamePanel(MainFrame mainFrame) {
 		
-		score = new JLabel("Score: 2");
+		scoreLabel = new JLabel("Score: 2");
+		add(scoreLabel);
 		
+		physicsEngine = new PhysicsEngine(MainFrame.X_SIZE, MainFrame.Y_SIZE);
 		
-		add(score);
+		Thread gameThread = new Thread(physicsEngine);
+		gameThread.start();
+		
 	}
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	
 	
 	
 }
--- src/com/hack/balls/swing/MainFrame.java
+++ src/com/hack/balls/swing/MainFrame.java
@@ -10,7 +10,8 @@ public class MainFrame extends JFrame implements ActionListener{
 	public static final String TITLE = "Balls of Fury";
 	
 	public static final String START_GAME = "START";
-	
+	public static final int X_SIZE = 500;
+	public static final int Y_SIZE = 500;
 	private StartPanel startPanel;
 	private GamePanel gamePanel;
 	
@@ -18,7 +19,7 @@ public class MainFrame extends JFrame implements ActionListener{
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle(TITLE);
-		setSize(400, 400);
+		setSize(X_SIZE, Y_SIZE);
 		
 		startPanel = new StartPanel(this);
 		add(startPanel);
