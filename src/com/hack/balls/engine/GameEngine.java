2dfc46387a85e90345fcdd4664591be239d91b8c
Gerald <geraldgfong@gmail.com>
2011-11-09 17:29:20 -0800

    New gui stuff

--- src/com/hack/balls/engine/GameEngine.java
+++ src/com/hack/balls/engine/GameEngine.java
@@ -1,17 +1,14 @@
 package com.hack.balls.engine;
 
-import javax.swing.JFrame;
-
-import com.hack.balls.swing.MainFrame;
+import com.hack.balls.swing.GameGUI;
 
 public class GameEngine {
 
 	private static void runGame() {
 		Runnable swingRun = new Runnable() {
 			public void run() {
-				MainFrame mainFrame = new MainFrame();
-				mainFrame.setVisible(true);
-				mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				GameGUI gameGUI = new GameGUI();
+				gameGUI.init();
 			}
 		};
 		Thread swingThread = new Thread(swingRun);
